5a1d473593ffdc3d033f60de8ef1b53b7468ea33
Ashgarth <ashgarth81@gmail.com>
2025-11-21 09:53:54 +0100

    Añadir proyecto Playwright pw-saucedemo

---
+++ pw-saucedemo/tests/login.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('login SauceDemo', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('#user-name').fill('standard_user');
+  await page.locator('#password').fill('secret_sauce');
+  await page.locator('#login-button').click();
+  await expect(page).toHaveURL(/.*inventory.html/);
+  await expect(page.locator('.inventory_list')).toBeVisible();
+});
+
